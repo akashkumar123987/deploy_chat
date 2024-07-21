5879ee91fbe6c2e0c2216dd88d7576eefa615841
Akash Kumar <ak9608450671@gmail.com>
2024-07-21 18:40:28 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ import { persistStore } from 'redux-persist';
 
 let persistor = persistStore(store);
 
-export const BASE_URL="https://deploy-chat.onrender.com"
+export const BASE_URL="deploy-chat-seven.vercel.app"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
